61fdfc2746915f5cf60feb834ee2f2fe59a3644f
1shefalika9 <shefalikag@outlook.com>
2020-07-30 14:26:28 +0530

    Second commit to the final R script of the course project

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,21 @@
+  #Loading required packages
 library(dplyr)
 
+  #downloading required dataset
+filename <- "projectfiles_UCI HAR Dataset.zip"
+
+  #Checking if zip file already exists.
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  download.file(fileURL, filename)
+}  
+
+  #Checking if folder exists
+if (!file.exists("UCI HAR Dataset")) { 
+  unzip(filename) 
+}
+
+   #reading 'features.txt' and 'activity_labels.txt' files
 features <- read.table("UCI HAR Dataset/features.txt", col.names = c("sno","function"))
 activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
 
